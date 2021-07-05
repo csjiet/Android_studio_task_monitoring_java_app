de428229ef782b5b1d5af593b48406a2f85d3b27
csjiet <cs.jiet@gmail.com>
2021-07-05 05:09:37 -0500

    updated 1. starting and 2.mode selecting activities using buttons to app

--- app/src/main/java/com/example/android_task_monitoring_java_app/MainActivity.java
+++ app/src/main/java/com/example/android_task_monitoring_java_app/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.android_task_monitoring_java_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button modeChoosingActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        modeChoosingActivityButton = (Button) findViewById(R.id.button);
+        modeChoosingActivityButton .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openModeChoosingActivity();
+            }
+        });
+
+    }
+
+    public void openModeChoosingActivity(){
+        Intent intent = new Intent(this, ModeChoosingActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/android_task_monitoring_java_app/ModeChoosingActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_task_monitoring_java_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModeChoosingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_choosing2);
+    }
+}
